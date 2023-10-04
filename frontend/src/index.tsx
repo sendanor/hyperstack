e6829acc5a74afee5dff04a35a87ff5dc9036f4c
Jaakko Heusala <jhh@hg.fi>
2023-10-04 23:00:11 +0300

    Initial app implementation: https://github.com/sendanor/hyperstack/issues/27

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,38 +1,37 @@
 // Copyright (c) 2022-2023. Heusala Group Oy <info@heusalagroup.fi>. All rights reserved.
 
 import { HelmetProvider } from "react-helmet-async";
+import { HgFrontend } from "./fi/hg/frontend/HgFrontend";
 import { HgReact } from "./fi/hg/frontend/HgReact";
 import { HgReactContext } from "./fi/hg/frontend/HgReactContext";
-import { HyperApp } from './fi/sendanor/hyperapp/components/apps/HyperApp';
-import { HgFrontend } from "./fi/hg/frontend/HgFrontend";
-import { HyperArticle } from "./fi/sendanor/hyperapp/components/article/HyperArticle";
+import { Hyper } from "./fi/sendanor/hyperapp/components/apps/Hyper";
+import { HyperRenderer } from "./fi/sendanor/hyperapp/renderers/HyperRenderer";
+import { HyperRendererImpl } from "./fi/sendanor/hyperapp/renderers/HyperRendererImpl";
+import { HyperDTO } from "./fi/sendanor/hyperstack/dto/HyperDTO";
+import { createCompleteOrderApp } from "./fi/sendanor/hyperstack/samples/order/CompleteOrderApp";
 import './fi/sendanor/hyperapp/styles/hyperapp-index.scss';
 
-const PUBLIC_URL = process.env.PUBLIC_URL ?? 'http://localhost:3000'
-const REACT_APP_LANGUAGE = process.env.REACT_APP_LANGUAGE ?? 'en'
+const PUBLIC_URL : string = process.env.PUBLIC_URL ?? 'http://localhost:3000';
+const REACT_APP_LANGUAGE : string = process.env.REACT_APP_LANGUAGE ?? 'en';
 
-const INDEX_ROUTE = '/';
-const ANY_ROUTE = '*';
+const MY_APP_NAME : string = 'OrderApp';
+
+const dto : HyperDTO = createCompleteOrderApp(
+    MY_APP_NAME,
+    PUBLIC_URL,
+    REACT_APP_LANGUAGE
+);
+
+const hyperRenderer : HyperRenderer = HyperRendererImpl.create();
 
 HgFrontend.initialize();
 HgReact.initialize(
     <HelmetProvider>
         <HgReactContext>
-            <HyperApp
-                publicUrl={PUBLIC_URL}
-                language={REACT_APP_LANGUAGE}
-                routeList={[
-                    {
-                        path: INDEX_ROUTE,
-                        view: [
-                            <HyperArticle><p>hello world</p></HyperArticle>
-                        ]
-                    },
-                    {
-                        path: ANY_ROUTE,
-                        redirect: INDEX_ROUTE
-                    },
-                ]}
+            <Hyper
+                app={MY_APP_NAME}
+                definitions={dto}
+                renderer={hyperRenderer}
             />
         </HgReactContext>
     </HelmetProvider>
