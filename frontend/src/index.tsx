11a3f96f42121aaeb78e1be3cf03406f8e7c8f82
Jaakko Heusala <jhh@hg.fi>
2023-10-29 07:52:46 +0200

    Refactored to use only single definition for app https://github.com/sendanor/hyperstack/issues/31

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -8,7 +8,7 @@ import { Hyper } from "./fi/sendanor/hyperapp/components/apps/Hyper";
 import { HyperRenderer } from "./fi/sendanor/hyperapp/renderers/HyperRenderer";
 import { HyperRendererImpl } from "./fi/sendanor/hyperapp/renderers/HyperRendererImpl";
 import { HyperDTO } from "./fi/sendanor/hyperstack/dto/HyperDTO";
-import { createCompleteOrderApp } from "./fi/sendanor/hyperstack/samples/order/CompleteOrderApp";
+import { createOrderAppDefinitions } from "./fi/sendanor/hyperstack/samples/order/OrderAppDefinition";
 import './fi/sendanor/hyperapp/styles/hyperapp-index.scss';
 
 const PUBLIC_URL : string = process.env.PUBLIC_URL ?? 'http://localhost:3000';
@@ -16,7 +16,7 @@ const REACT_APP_LANGUAGE : string = process.env.REACT_APP_LANGUAGE ?? 'en';
 
 const MY_APP_NAME : string = 'OrderApp';
 
-const dto : HyperDTO = createCompleteOrderApp(
+const dto : HyperDTO = createOrderAppDefinitions(
     MY_APP_NAME,
     PUBLIC_URL,
     REACT_APP_LANGUAGE
@@ -29,7 +29,6 @@ HgReact.initialize(
     <HelmetProvider>
         <HgReactContext>
             <Hyper
-                app={MY_APP_NAME}
                 definitions={dto}
                 renderer={hyperRenderer}
             />
